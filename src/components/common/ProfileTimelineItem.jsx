c9c03e68d6d1570c740f629be283d371ba47ed78
DOF1109 <friasdaniel.1109@gmail.com>
2024-04-24 20:15:15 -0300

    Se finaliza el footer y se modifican estilos en el timeline del Profile

--- src/components/common/ProfileTimelineItem.jsx
+++ src/components/common/ProfileTimelineItem.jsx
@@ -16,22 +16,20 @@ const ProfileTimelineItem = ({ title, text, url, hasConnector }) => {
       </TimelineSeparator>
       <TimelineContent>
         <Typography>{title}: </Typography>
-        <Typography color="text.secondary">
-          <Link
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-            underline="none"
-            color="text.secondary"
-            sx={{
-              '&:hover': {
-                color: 'primary.main',
-              }
-            }}
-          >
-            {text}
-          </Link>
-        </Typography>
+        <Link
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          underline="none"
+          color="text.secondary"
+          sx={{
+            "&:hover": {
+              color: "primary.main",
+            },
+          }}
+        >
+          {text}
+        </Link>
       </TimelineContent>
     </TimelineItem>
   );
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,21 +1,28 @@
-import { Box, Typography } from "@mui/material"
-import resumeData from "../../utils/resumeData"
+import { Box, Typography } from "@mui/material";
+import resumeData from "../../utils/resumeData";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <Box 
+    <Box
       bgcolor="background.paper"
       borderRadius={2}
       boxShadow={5}
       display="flex"
+      alignItems="center"
       justifyContent="space-between"
       px={3}
       py={2}
-      >
-      <Typography variant="body2">{resumeData.name}</Typography>
-      <Typography variant="body2">© 2024</Typography>
+    >
+      <Typography variant="body1">{resumeData.name}</Typography>
+      <Typography variant="caption" color="text.secondary">
+        Desarrollado por
+        <Link className="clear-link" to="/">
+          <Typography variant="caption" color="primary"> {resumeData.name}</Typography>
+        </Link>
+      </Typography>
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
