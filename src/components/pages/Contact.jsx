06b01c3348acb4ce19ef47fd73bc358f69db8f9b
DOF1109 <friasdaniel.1109@gmail.com>
2024-04-27 14:29:13 -0300

    Se instala formik y yup

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,12 +1,18 @@
-import { Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import SectionTitle from "../common/SectionTitle";
 
 const Contact = () => {
   return (
-    <Box component="section">
-        <SectionTitle text="Contacto" />
-        
-    </Box>
+    <Grid container spacing={3} component="section">
+        <Grid item xs={12} sm={6}>
+            <SectionTitle text="Formulario de contacto" />
+
+        </Grid>
+        <Grid item xs={12} sm={6}>
+            <SectionTitle text="Información de contacto" />
+            
+        </Grid>
+    </Grid>
   )
 }
 
