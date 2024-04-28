f4748f1f8af117d4784dfecb3655420edfc5cbd0
DOF1109 <friasdaniel.1109@gmail.com>
2024-04-28 11:41:18 -0300

    Se estructura Contact para implementar formularios

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,13 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import SectionTitle from "../common/SectionTitle";
 
 const Contact = () => {
   return (
     <Box component="section">
-        <SectionTitle text="Contacto" />
-        
-    </Box>
-  )
-}
+      <Grid container spacing={3} component="section">
+        <Grid item xs={12} sm={6}>
+          <SectionTitle text="Formulario de contacto" />
 
-export default Contact
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <SectionTitle text="Información de contacto" />
+          
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Contact;
