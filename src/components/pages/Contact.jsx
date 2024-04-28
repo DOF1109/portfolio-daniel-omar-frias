6ef18bebc754296f6cba153d0602dbad6ae8e1a0
DOF1109 <friasdaniel.1109@gmail.com>
2024-04-28 11:58:16 -0300

    Se instala Formik y YUP y se ejecuta "npm audit fix" . Se estructura el formulario con MUI solamente

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid, TextField } from "@mui/material";
 import SectionTitle from "../common/SectionTitle";
 
 const Contact = () => {
@@ -7,11 +7,36 @@ const Contact = () => {
       <Grid container spacing={3} component="section">
         <Grid item xs={12} sm={6}>
           <SectionTitle text="Formulario de contacto" />
-
+          <Box component="form" onSubmit={() => {}}>
+            <TextField
+              name="name"
+              label="Nombre"
+              variant="outlined"
+              onChange={() => {}}
+            />
+            <TextField
+              name="email"
+              label="Email"
+              variant="outlined"
+              onChange={() => {}}
+            />
+            <TextField
+              name="message"
+              label="Mensaje"
+              variant="outlined"
+              onChange={() => {}}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ borderRadius: "50px" }}
+            >
+              Enviar
+            </Button>
+          </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
           <SectionTitle text="Información de contacto" />
-          
         </Grid>
       </Grid>
     </Box>
