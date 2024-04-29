968f3648201ed1d2e25a4c545a87771d76b0b90a
DOF1109 <friasdaniel.1109@gmail.com>
2024-04-29 14:53:18 -0300

    Revert "Se instala SweetAlert2"

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,8 +2,11 @@ import { Box, Button, Grid, TextField } from "@mui/material";
 import SectionTitle from "../common/SectionTitle";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import Swal from "sweetalert2";
+import useTheme from "@mui/material/styles/useTheme";
 
 const Contact = () => {
+  const theme = useTheme();
   const { handleChange, handleSubmit, errors } = useFormik({
     initialValues: {
       name: "",
@@ -11,10 +14,27 @@ const Contact = () => {
       message: "",
     },
     onSubmit: (data) => {
-      console.log(data);
+      Swal.fire({
+        title: "¿Confirmar envío?",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Enviado",
+            icon: "success",
+            timer: 3000,
+          });
+          console.log(data);
+        }
+      });
     },
     validationSchema: Yup.object({
       name: Yup.string()
+        .trim()
         .min(3, "El nombre debe tener al menos 3 caracteres")
         .max(50, "El nombre debe tener como maxímo 50 caracteres")
         .required("Complete este campo"),
@@ -22,6 +42,7 @@ const Contact = () => {
         .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "Ingrese un email válido")
         .required("Complete este campo"),
       message: Yup.string()
+        .trim()
         .min(4, "El mensaje debe tener al menos 4 caracteres")
         .required("Complete este campo"),
     }),
@@ -33,12 +54,7 @@ const Contact = () => {
       <Grid container spacing={4} component="section">
         <Grid item xs={12} sm={6}>
           <SectionTitle text="Formulario de contacto" />
-          <Grid
-            container
-            spacing={3}
-            component="form"
-            onSubmit={handleSubmit}
-          >
+          <Grid container spacing={3} component="form" onSubmit={handleSubmit}>
             <Grid item xs={12} lg={10} mt={2}>
               <TextField
                 name="name"
