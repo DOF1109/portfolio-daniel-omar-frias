099126e6970cde57530074315530b9947dc41621
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-07 18:08:47 -0300

    Se agrega slide, faltan cargar las imagenes dentro del mismo

--- src/components/common/ImagesCarrousel.jsx
+++ src/components/common/ImagesCarrousel.jsx
@@ -1,5 +1,4 @@
-
-import { forwardRef } from "react";
+import { forwardRef, useRef, useEffect } from "react";
 import {
   AppBar,
   Dialog,
@@ -9,12 +8,32 @@ import {
   Typography,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import { register } from "swiper/element/bundle";
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
+register();
+
 const ImagesCarrousel = ({ openCarrousel, onClose, title, images }) => {
+  const swiperElRef = useRef(null);
+
+  useEffect(() => {
+    if (swiperElRef.current) {
+      // listen for Swiper events using addEventListener
+      swiperElRef.current.addEventListener("swiperprogress", (e) => {
+        const [swiper, progress] = e.detail;
+        console.log(progress);
+      });
+  
+      swiperElRef.current.addEventListener("swiperslidechange", (e) => {
+        console.log("slide changed");
+      });
+    }
+  }, []);
+  
+
   return (
     <Dialog
       fullScreen
@@ -37,7 +56,16 @@ const ImagesCarrousel = ({ openCarrousel, onClose, title, images }) => {
           </Typography>
         </Toolbar>
       </AppBar>
-      {/* Add slider */}
+      <swiper-container
+        ref={swiperElRef}
+        slides-per-view="3"
+        navigation="true"
+        pagination="true"
+      >
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+      </swiper-container>
     </Dialog>
   );
 };
