5bbee2390fa6e28e8b48eb8d4b07b2fcad43cec7
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-07 17:55:37 -0300

    Se instala "Swiper"

--- src/components/common/ImagesCarrousel.jsx
+++ src/components/common/ImagesCarrousel.jsx
@@ -1,9 +1,4 @@
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/effect-coverflow";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
-// import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+
 import { forwardRef } from "react";
 import {
   AppBar,
