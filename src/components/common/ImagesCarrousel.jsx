a84cf82baca0cabd1e37bb8422b0456fe65dbea8
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-08 15:33:32 -0300

    Se desinstala "Swiper"

--- src/components/common/ImagesCarrousel.jsx
+++ src/components/common/ImagesCarrousel.jsx
@@ -1,7 +1,6 @@
-import { forwardRef, useRef, useEffect } from "react";
+import { forwardRef } from "react";
 import {
   AppBar,
-  Box,
   Dialog,
   IconButton,
   Slide,
@@ -9,37 +8,12 @@ import {
   Typography,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-// register();
-
 const ImagesCarrousel = ({ openCarrousel, onClose, title, images }) => {
-  // const swiperElRef = useRef(null);
-
-  // useEffect(() => {
-  //   if (swiperElRef.current) {
-  //     // listen for Swiper events using addEventListener
-  //     swiperElRef.current.addEventListener("swiperprogress", (e) => {
-  //       const [swiper, progress] = e.detail;
-  //       console.log(progress);
-  //     });
-  
-  //     swiperElRef.current.addEventListener("swiperslidechange", (e) => {
-  //       console.log("slide changed");
-  //     });
-  //   }
-  // }, []);
-  
-
   return (
     <Dialog
       fullScreen
@@ -62,58 +36,7 @@ const ImagesCarrousel = ({ openCarrousel, onClose, title, images }) => {
           </Typography>
         </Toolbar>
       </AppBar>
-      {/* <swiper-container
-        ref={swiperElRef}
-        slides-per-view="1"
-        space-between="10"
-        navigation="true"
-        pagination="true"
-        centered-slides="true"
-      >
-        {images.map((image, index) => {
-          return (
-            <swiper-slide key={index}>
-              <Box 
-                component="img" 
-                src={image} 
-                sx={{
-                  maxWidth: "90vw", 
-                  maxHeight: "90vh", 
-                  objectFit: "contain",
-                  transition: "transform 0.5s ease-in-out"
-                }} 
-              />
-            </swiper-slide>
-          )
-        })}
-      </swiper-container> */}
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        // spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        {images.map((image, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <Box 
-                component="img" 
-                src={image} 
-                sx={{
-                  maxWidth: "90vw", 
-                  maxHeight: "90vh", 
-                  objectFit: "contain",
-                  transition: "transform 0.5s ease-in-out"
-                }} 
-              />
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
+      {/* Add slider */}
     </Dialog>
   );
 };
