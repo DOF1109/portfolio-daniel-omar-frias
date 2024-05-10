0fecab9fe12b0db2df36a442eb3f34cd755df9b4
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-10 13:28:28 -0300

    Se agrega boton flotante para ir hacia arriba

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Box } from "@mui/material";
+import { Box, Fab } from "@mui/material";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useEffect, useState } from "react";
 
 const Layout = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const checkScroll = () => {
+    if (!isVisible && window.scrollY > 400){
+      setIsVisible(true);
+    } else if (isVisible && window.scrollY <= 400){
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScroll);
+    return () => window.removeEventListener("scroll", checkScroll);
+  }, [isVisible]);
+
   return (
-    <div>
+    <>
       <Header />
       <Box
         component="main"
@@ -16,9 +33,20 @@ const Layout = () => {
         my={3}
       >
         <Outlet />
+        {isVisible && (
+          <Fab 
+            size="medium" 
+            color="primary" 
+            aria-label="Arriba" 
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+            sx={{ position:"fixed", bottom: "1.5rem", right: { xs:"1rem", sm:"1.5rem", md:"2rem" } }}
+          >
+            <KeyboardArrowUpIcon />
+          </Fab>
+        )}
       </Box>
       <Footer />
-    </div>
+    </>
   );
 };
 
