a7c41fc0044753804061774885c53f12cefbc627
DOF1109 <friasdaniel.1109@gmail.com>
2024-05-11 15:47:06 -0300

    Se optimiza formato de imagen

--- src/assets/images/projects/soundRentals/soundRentalsIndex.js
+++ src/assets/images/projects/soundRentals/soundRentalsIndex.js
@@ -1,2 +1,2 @@
 export { default as SoundRentals_1 } from "./SoundRentals-1.webp";
-export { default as SoundRentals_2 } from "./SoundRentals-2.png";
+export { default as SoundRentals_2 } from "./SoundRentals-2.webp";
