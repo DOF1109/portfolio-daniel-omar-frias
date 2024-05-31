0010e21cb96904ef14b41b2d57f00f34c476604d
Daniel Omar Frias <friasdaniel.1109@gmail.com>
2024-05-30 23:35:21 -0300

    Se actualiza CV e imagenes de SoundRentals

--- src/assets/images/projects/soundRentals/soundRentalsIndex.js
+++ src/assets/images/projects/soundRentals/soundRentalsIndex.js
@@ -1,2 +1,3 @@
 export { default as SoundRentals_1 } from "./SoundRentals-1.webp";
 export { default as SoundRentals_2 } from "./SoundRentals-2.webp";
+export { default as SoundRentals_3 } from "./SoundRentals-3.webp";
